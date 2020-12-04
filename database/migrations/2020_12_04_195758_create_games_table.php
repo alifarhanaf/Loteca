94b496b62aa6d9acaf0d18c3c2145d41f2f0cf59
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2020-12-05 01:00:55 +0500

    RoundsModule

--- database/migrations/2020_12_04_195758_create_games_table.php
+++ database/migrations/2020_12_04_195758_create_games_table.php
@@ -15,6 +15,9 @@ class CreateGamesTable extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('team_a');
+            $table->string('team_b');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2020_12_04_195819_create_rounds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoundsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('starting_date');
+            $table->string('ending_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rounds');
+    }
+}
