6f9b4498bf2463dfce0496f367e4a2f265b8df9d
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-02-18 23:46:20 +0500

    up

--- app/Http/Controllers/Api/CoinController.php
+++ app/Http/Controllers/Api/CoinController.php
@@ -10,14 +10,25 @@ class CoinController extends Controller
 {
     public function index(Request $request){
         $user =  User::where('email',$request->email)->first();
-        $user->contacts;
-        $data = array(
+        if (count($user) > 0 ){
+            $user->contacts;
+            $data = array(
             "status"=>200,
             "response"=>"true",
             "message" => "Success",
             "user" => $user,
             );
             return response()->json($data,200);
+
+        }else{
+            $data = array(
+            "status" => 404,
+            "response" => "false",
+            "message" => "User Not Found",
+            );
+            return response()->json($data,200);
+        }
+        
         
     }
 }
