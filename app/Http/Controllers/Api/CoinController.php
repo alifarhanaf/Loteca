9e57e4394ad0c8039e3f3d6b707dfb1d0ca37f4c
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-02-21 18:37:51 +0500

    jj

--- app/Http/Controllers/Api/CoinController.php
+++ app/Http/Controllers/Api/CoinController.php
@@ -14,6 +14,7 @@ class CoinController extends Controller
         $user =  User::where('email',$request->email)->first();
         if ($user ){
             $user->contacts;
+            $user->images;
             $data = array(
             "status"=>200,
             "response"=>"true",
