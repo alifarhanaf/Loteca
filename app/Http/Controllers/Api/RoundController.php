380c32cca7f2de82c52fdfdb3c870554ca7a5e4b
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-02-24 22:51:58 +0500

    f

--- app/Http/Controllers/Api/RoundController.php
+++ app/Http/Controllers/Api/RoundController.php
@@ -516,7 +516,7 @@ class RoundController extends Controller
          
             $gm = Game::where('id',$UA->game_id)->first();
             $gameAnswer0 = $gm->results->Answer;
-            dd($gameAnswer0);
+            dd($gameAnswer0 . $UA->answer);
             if($gameAnswer0 == $UA->answer){
                 
                 $i++;
