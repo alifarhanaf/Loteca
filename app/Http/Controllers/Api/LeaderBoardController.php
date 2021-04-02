41f30295c26a30e867ae56a3cb79e6d941c3d5f3
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-04-02 19:31:19 +0500

    hh

--- app/Http/Controllers/Api/LeaderBoardController.php
+++ app/Http/Controllers/Api/LeaderBoardController.php
@@ -107,7 +107,7 @@ class LeaderBoardController extends Controller
                     
               
             }
-            // return $multipleWinners;
+            return $multipleWinners;
           
             $multipleWinnersMonthly = [];
      
