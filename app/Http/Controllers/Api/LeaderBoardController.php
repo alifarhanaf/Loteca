89f452ea1b758122cfa2fb99eb2fccf4467d7bfe
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-04-02 20:14:04 +0500

    gf

--- app/Http/Controllers/Api/LeaderBoardController.php
+++ app/Http/Controllers/Api/LeaderBoardController.php
@@ -95,8 +95,8 @@ class LeaderBoardController extends Controller
             // }
             for ($i = 0; $i < count($points); $i++) {
                 $aa = DB::table('points')->where('user_id',$points[$i]->user_id)->get();
-                $image = DB::table('images')->where('user_id',$points[$i]->user_id)->first();
-                return $image;
+                $image = DB::table('images')->where('user_id',$points[$i]->user_id)->get();
+                dd($image);
                 // $aa = Point::where('user_id',$points[$i]->user_id)->get();
                 $count = 0;
                 foreach($aa as $a){
