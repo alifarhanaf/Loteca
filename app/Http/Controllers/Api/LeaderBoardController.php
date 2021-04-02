542bb81e28e8674e2f148ceef7c6f9c5193c11fc
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-04-02 19:58:09 +0500

    gg

--- app/Http/Controllers/Api/LeaderBoardController.php
+++ app/Http/Controllers/Api/LeaderBoardController.php
@@ -95,12 +95,13 @@ class LeaderBoardController extends Controller
             // }
             for ($i = 0; $i < count($points); $i++) {
                 $aa = DB::table('points')->where('user_id',$points[$i]->user_id)->get();
+                $user = User::where('id',$points[$i]->user_id)->with('images')->first();
                 // $aa = Point::where('user_id',$points[$i]->user_id)->get();
                 $count = 0;
                 foreach($aa as $a){
                     $count  = $count + $a->points;
                 }
-                $points[$i]->user['image'] = $points[$i]->user->images[0]->url;
+                $points[$i]->user['image'] = $user->images[0]->url;
                 $points[$i]->user['Winning Coins'] = $count*10;
                 if(!in_array($points[$i]->user, $multipleWinners, true)){
                     array_push($multipleWinners,$points[$i]->user);
