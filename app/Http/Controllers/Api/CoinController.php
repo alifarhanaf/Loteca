c933daef642c04016101a76b0d5e9267a233c272
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-04-07 18:39:36 +0500

    chk

--- app/Http/Controllers/Api/CoinController.php
+++ app/Http/Controllers/Api/CoinController.php
@@ -110,6 +110,7 @@ class CoinController extends Controller
         $i = 0;
         foreach($coinsTransfer as $ct){
             $user = User::where('id',$ct->receiver_id)->first();
+            dd($user,$user->contacts);
             $arr[$i]['user_email'] = $user->contacts[0]->email;
             $arr[$i]['user_phone'] = $user->contacts[0]->phone;
             $arr[$i]['user_whatsapp'] = $user->contacts[0]->whatsapp;
