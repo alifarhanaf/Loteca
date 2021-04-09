f491efce203df3d3d09d3f09b194dd209f8c1d41
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-04-09 21:36:57 +0500

    ja

--- resources/views/roundDetailScreen.blade.php
+++ resources/views/roundDetailScreen.blade.php
@@ -74,10 +74,10 @@
      @foreach ($firstRoundWinners as $item)
          
      
-      <div class="col-md-2 ">
+      <div class="col-md-3 ">
           <div class="text-center card-box" style="margin-bottom: 0px;">
               <div class="member-card pt-2 pb-2">
-                  <div class="thumb-lg member-thumb mx-auto"><img src={{$item['images']['0']['url']}} class="rounded-circle img-thumbnail" alt="profile-image"></div>
+                  <div class="thumb-lg member-thumb mx-auto" ><img style="width: 180px;height:180px;" src={{$item['images']['0']['url']}} class="rounded-circle img-thumbnail" alt="profile-image"></div>
                   <div class="">
                       <h4>{{$item['name']}}</h4>
                       <p class="text-muted">End User <span>| </span><span><a href="#" class="text-pink">Loteca 2.0</a></span></p>
@@ -124,10 +124,10 @@
  @foreach ($secondRoundWinners as $item)
      
  
-  <div class="col-md-2 ">
+  <div class="col-md-3 ">
       <div class="text-center card-box" style="margin-bottom: 0px;">
           <div class="member-card pt-2 pb-2">
-              <div class="thumb-lg member-thumb mx-auto"><img src={{$item['images']['0']['url']}} class="rounded-circle img-thumbnail" alt="profile-image"></div>
+              <div class="thumb-lg member-thumb mx-auto"><img style="width: 180px;height:180px;" src={{$item['images']['0']['url']}} class="rounded-circle img-thumbnail" alt="profile-image"></div>
               <div class="">
                   <h4>{{$item['name']}}</h4>
                   <p class="text-muted">End User <span>| </span><span><a href="#" class="text-pink">Loteca 2.0</a></span></p>
@@ -174,10 +174,10 @@
  @foreach ($thirdRoundWinners as $item)
      
  
-  <div class="col-md-2 ">
+  <div class="col-md-3 ">
       <div class="text-center card-box" style="margin-bottom: 0px;">
           <div class="member-card pt-2 pb-2">
-              <div class="thumb-lg member-thumb mx-auto"><img src={{$item['images']['0']['url']}} class="rounded-circle img-thumbnail" alt="profile-image"></div>
+              <div class="thumb-lg member-thumb mx-auto"><img style="width: 180px;height:180px;" src={{$item['images']['0']['url']}} class="rounded-circle img-thumbnail" alt="profile-image"></div>
               <div class="">
                   <h4>{{$item['name']}}</h4>
                   <p class="text-muted">End User <span>| </span><span><a href="#" class="text-pink">Loteca 2.0</a></span></p>
