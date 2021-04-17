cad28d3f99eeeca30d71f95873e591accd83077f
Farhan Ali <71764231+alifarhanaf1@users.noreply.github.com>
2021-04-17 21:20:53 +0500

    jj

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -13,7 +13,7 @@ class ProfileController extends Controller
 {
     public function updateUser(Request $request){
         // dd($request->image);
-        return $request;
+        dd($request) ;
 
         $user = Auth::user();
         
